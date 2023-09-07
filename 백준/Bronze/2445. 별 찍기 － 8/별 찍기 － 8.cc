9e0c68aa9084e09528908a461e13143447f26ba5
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:15:11 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const double PI = acos(-1);
+
+int main(){
+	int n; cin >> n;
+	for(int i=1; i<n; i++){
+		for(int j=0; j<2*n; j++){
+			if(j<i || j > 2*n-i-1) cout << '*';
+			else cout << ' ';
+		}
+		cout << '\n';
+	}
+	for(int i=n; i>0; i--){
+		for(int j=0; j<2*n; j++){
+			if(j<i || j > 2*n-i-1) cout << '*';
+			else cout << ' ';
+		}
+		if(i > 1) cout << '\n';
+	}
+	return 0;
+}
