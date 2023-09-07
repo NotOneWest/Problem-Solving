2d2c43ed67b4fe02243912047768db5028e37a9a
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:12:48 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n; cin >> n;
+	queue<int> q;
+	for(int i=1; i<=n; i++) q.push(i);
+	while(q.size() != 1){
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	printf("%d\n", q.front());
+	return 0;
+}
