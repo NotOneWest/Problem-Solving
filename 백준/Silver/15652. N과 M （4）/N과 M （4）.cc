af7e2e3229708a4b2e7a812e82fd6ce745dd67b6
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:54:21 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const double PI = acos(-1);
+
+int n,m,sequence[9]={0,};
+
+void dfs(int start, int cnt){
+  if(cnt==m){
+    for(int i=0; i<m; i++){
+      cout << sequence[i] << ' ';
+    }
+    cout << '\n';
+  } else{
+    for(int i=start; i<=n; i++){
+      sequence[cnt]=i;
+      dfs(i,cnt+1);
+    }
+  }
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+  cin >> n >> m;
+  dfs(1,0);
+	return 0;
+}
