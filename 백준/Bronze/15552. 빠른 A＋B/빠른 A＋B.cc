23da344c226304f702bfd3779711ee6fd4eeec5f
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:53:46 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 244 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	int t;
+	cin >> t;
+	for(int i=0; i<t; i++){
+		int a,b;
+		cin >>a >>b;
+		cout << a+b << '\n';
+	}
+	return 0;	
+}
