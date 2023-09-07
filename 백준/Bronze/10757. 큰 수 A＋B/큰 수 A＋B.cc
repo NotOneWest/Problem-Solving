769c5e9806fe1604c0e376e411f3faf9773bdb60
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:40:22 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 4 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string a,b,res;
+	cin >> a >> b;
+	int size = a.size(), carry = 0;
+	if(size > b.size()){
+		int size2 = b.size();
+		for(int i=0; i<size-size2; i++) b = '0' + b;
+	}
+	else if(size < b.size()) {
+		for(int i=0; i<b.size()-size; i++) a = '0' + a;
+		size = b.size();
+	}
+	for(int i=size-1; i>=0; i--){
+		string c;
+		c = to_string(a[i] + b[i] - '0' - '0' + carry);
+		if(c.size() == 2){
+			carry = c[0]-'0';
+			res = c[1] + res;
+		}
+		else{
+			res = c[0] + res;
+			carry = 0;
+		}
+	}
+	if (carry == 1) res = to_string(carry) + res;
+	
+	printf("%s", res.c_str());
+	return 0;	
+}
