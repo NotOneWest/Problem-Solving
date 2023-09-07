9cd92eccab4ea8aafa86420612441c0bac802c14
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:47:26 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 36 ms, Memory: 5344 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const double PI = acos(-1);
+
+vector<pii> vertex[20001];
+int dis[20001], inf=999999999, visited[20001]={0,};
+
+void dijkstra(int start){
+  dis[start]=0;
+  priority_queue<pii> pq;
+  pq.push(mp(0, start));
+  while(!pq.empty()){
+    int cur=pq.top().second, curdis=-pq.top().first;
+    pq.pop();
+    if(dis[cur]<curdis) continue;
+    for(int i=0; i<vertex[cur].size(); i++){
+      int next=vertex[cur][i].second, nextdis=vertex[cur][i].first+curdis;
+      if(dis[next]>nextdis){
+        dis[next]=nextdis;
+        visited[next]=cur;
+        pq.push(mp(-nextdis, next));
+      }
+    }
+  }
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+  int n,m; cin >> n >> m;
+  for(int i=0; i<m; i++){
+    int u,v,w; cin >> u >> v >> w;
+    vertex[u].pb(mp(w,v));
+  }
+  int start, end; cin >> start >> end;
+  for(int i=0; i<=20001; i++) dis[i]=inf;
+  dijkstra(start);
+  cout << dis[end] << '\n';
+  vector<int> ans;
+  int t=end;
+  while(t!=0){
+    ans.pb(t);
+    t=visited[t];
+  }
+  cout << ans.size() << '\n';
+  for(int i=1; i<=ans.size(); i++) cout << ans[ans.size()-i] << ' ';
+  cout << '\n';
+	return 0;
+}
