2728f9256f6b5937ac07c59a9f012e037dc91e32
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:28:30 +0900

    [Unrated] Title: 더하기, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end();
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const double PI = acos(-1);
+
+int main(){
+	int a,b; cin >> a >> b;
+ 	cout<< a+b << endl;
+	return 0;
+}
