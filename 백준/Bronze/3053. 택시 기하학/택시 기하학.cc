47177c5d69fa8715bd678f6ed8dff3d3b6e29dee
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:24:55 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define M_PI 3.14159265358979323846
+int main(){
+	double r; cin >> r;
+	printf("%f\n", r*r*M_PI);
+	printf("%f", 2*r*r);
+	return 0;
+}
