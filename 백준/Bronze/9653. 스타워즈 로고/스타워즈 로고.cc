90d0b09df1625fd26be8f95bdf8fe45d1a9ca8b9
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:35:02 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end();
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const double PI = acos(-1);
+
+int main(){
+	printf("    8888888888  888    88888\n");
+	printf("   88     88   88 88   88  88\n");
+	printf("    8888  88  88   88  88888\n");
+	printf("       88 88 888888888 88   88\n");
+	printf("88888888  88 88     88 88    888888\n");
+	printf("\n");
+	printf("88  88  88   888    88888    888888\n");
+	printf("88  88  88  88 88   88  88  88\n");
+	printf("88 8888 88 88   88  88888    8888\n");
+	printf(" 888  888 888888888 88  88      88\n");
+	printf("  88  88  88     88 88   88888888\n");
+	return 0;
+}
