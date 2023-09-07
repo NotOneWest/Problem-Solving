2fa8d796d3d01a0bf0ca7acbf4e2fe2799d6731e
HanSeo An <42708815+NotOneWest@users.noreply.github.com>
2023-09-07 14:58:56 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const double PI = acos(-1);
+
+int main(){
+	ll n; cin >> n;
+	cout << 4*n;
+	return 0;
+}
