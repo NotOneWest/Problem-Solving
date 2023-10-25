bd17a2117271220f224e1c501645a1b8c2c0a4a6
HanSeo An <gkstj0604@naver.com>
2023-10-25 10:01:38 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.04 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long MOD = 1234567;
+long long dp[2001];
+
+long long solution(int n) {
+    long long answer = 0;
+    dp[0] = 0; dp[1] = 1; dp[2] = 2;
+    for(int i=3; i<=n; i++) dp[i] = (dp[i-1] + dp[i-2])%MOD;
+    answer = dp[n];
+    return answer;
+}
