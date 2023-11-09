de0ca68307e503d9e6e0b32a08c15be2894d689e
HanSeo An <gkstj0604@gmail.com>
2023-11-09 15:11:30 +0900

    Update 등대.cpp

--- 프로그래머스/3/133500. 등대/등대.cpp
+++ 프로그래머스/3/133500. 등대/등대.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int answer = 0;
 bool light[100001] = {false, };
 
-void dfs(int node, int parent, vector<vector<int>> &info){
+void dfs(int node, int parent, vector<vector<int>> &info){ 
+    // info vector를 참조(&)로 넘기지 않으면 복사하면서 넘겨서 시간 초과가 난다...
+    // Data 양이 많다면 Parameter를 pass by value 대신 reference로 ... (복사 X -> 시간 절약)
     for(int i=0; i<info[node].size(); i++){
         if(info[node][i] != parent){
             dfs(info[node][i], node, info);
