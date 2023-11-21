b2439762da3d9a6ed78dd280aa69c596f46c545e
HanSeo An <gkstj0604@naver.com>
2023-11-21 14:35:08 +0900

    [level 3] Title: 숫자 타자 대회, Time: 77.46 ms, Memory: 49.6 MB -BaekjoonHub

--- 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
+++ 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 vector<pair<int, int>> keys;
 int dp[100001][10][10];
-string nums;
 
 void setLoc(){
     keys.push_back(make_pair(3, 1));
@@ -22,17 +21,17 @@ int getW(int now, int move){
     return min(dx, dy)*3 + abs(dx-dy)*2;
 }
 
-int typing(int left, int right, int idx){
-    if(idx >= nums.length()) return 0;
+int typing(int left, int right, int idx, const string& numbers){
+    if(idx >= numbers.length()) return 0;
     
     if(left == right) return 9999999; // 왼손, 오른손 같으면 오류
     if(dp[idx][left][right] != 0) return dp[idx][left][right]; // 중복 제거
     
-    int leftW = getW(left, nums[idx]-'0');
-    int rightW = getW(right, nums[idx]-'0');
+    int leftW = getW(left, numbers[idx]-'0');
+    int rightW = getW(right, numbers[idx]-'0');
     
-    int w1 = leftW + typing(nums[idx]-'0', right, idx+1); // 왼손 이동한 경우
-    int w2 = rightW + typing(left, nums[idx]-'0', idx+1); // 오른손 이동한 경우
+    int w1 = leftW + typing(numbers[idx]-'0', right, idx+1, numbers); // 왼손 이동한 경우
+    int w2 = rightW + typing(left, numbers[idx]-'0', idx+1, numbers); // 오른손 이동한 경우
     
     dp[idx][left][right] = min(w1, w2);
     return dp[idx][left][right];
@@ -40,12 +39,12 @@ int typing(int left, int right, int idx){
 
 int solution(string numbers) {
     int answer = 0;
-    setLoc(); nums = numbers;
+    setLoc();
     for(int i=0; i<numbers.length(); i++){
         for(int j=0; j<10; j++){
             for(int k=0; k<10; k++) dp[i][j][k] = 0;
         }
     }
-    answer = typing(4, 6, 0);
+    answer = typing(4, 6, 0, numbers);
     return answer;
 }
