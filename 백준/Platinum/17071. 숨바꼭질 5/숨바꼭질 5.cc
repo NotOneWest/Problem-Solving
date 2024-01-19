ada572ac920c3c9d8a01e5ccb2219e826c4b43f8
HanSeo An <gkstj0604@naver.com>
2024-01-19 11:49:22 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 12 ms, Memory: 6864 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -1,64 +1,67 @@
-#include <bits/stdc++.h>
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-
+#include<iostream>
+#include<algorithm>
+#include<queue>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-const double PI = acos(-1);
+int even_sec[500002], odd_sec[500002]; // 특정 거리에 도달하는 시간
 
-int odd[500002], even[500002], cnt=0;
-queue<int> q;
-
-void bfs(int n){
+void bfs(int n) {
+	queue<int> q;
 	q.push(n);
-	even[n]=0;
-	while(!q.empty()){
-		int size = q.size();
-		for(int i=0; i<size; i++){
-			int temp = q.front();
-			int dt[3]={temp-1, temp+1, temp*2};
-			for(int p=0; p<3; p++){
-				if(0<=dt[p] && dt[p]<=500000){
-					if(cnt%2!=0){
-						if(even[dt[p]]==0){
-							q.push(dt[p]);
-							even[dt[p]]=cnt+1;
+	even_sec[n] = 0;
+	int size, t = 0;
+	while (!q.empty()) {
+		size = q.size();
+		// 매 시간에 위치할 수 있는 좌표에서 계산
+		for (int j = 0; j < size; j++) {
+			int x = q.front();
+			// 갈 수 있는 좌표 x-1, x+1, x*2
+			int dx[3] = { x - 1, x + 1, x * 2 };
+			q.pop();
+			for (int i = 0; i < 3; i++) {
+				if (0 <= dx[i] && dx[i] <= 500000) {
+					// t+1초가 들어가서 t가 홀수일 때 짝수초
+					if (t % 2 != 0) {
+						if (even_sec[dx[i]] == 0) {
+							q.push(dx[i]);
+							even_sec[dx[i]] = t + 1;
 						}
 					}
-					else{
-						if(odd[dt[p]]==0){
-							q.push(dt[p]);
-							odd[dt[p]]=cnt+1;
+					else {
+						if (odd_sec[dx[i]] == 0) {
+							q.push(dx[i]);
+							odd_sec[dx[i]] = t + 1;
 						}
-					}	
+					}
 				}
 			}
-			q.pop();
 		}
-		cnt++;
+		t++;
 	}
 }
 
-int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n,k,z=0,sec=500000; cin >> n >> k;
-	bfs(n);
-	if(n==k) sec=0;
-	while(k<=500000){
-		if(z%2==0){
-			if(even[k]<=z && even[k]>0) sec = min(sec,z);
-		}
-		else{
-			if(odd[k]<=z && odd[k]>0) sec = min(sec,z);
-		}
-		z++;
-		k+=z;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int n, k; cin >> n >> k;
+
+	if (n == k) {
+		cout << 0;
+		return 0;
 	}
-	if(sec == 500000) sec=-1;
-	cout << sec;
+
+	bfs(n); // 미리 시간 계산
+	int ans = 500010, t = 0;
+	// 매초 동생의 좌표와 시간 비교
+	while (k <= 500000) {
+		if (t % 2 == 0) {
+			if (0 < even_sec[k] && even_sec[k] <= t) ans = min(ans, t);
+		}
+		else {
+			if (0 < odd_sec[k] && odd_sec[k] <= t) ans = min(ans, t);
+		}
+		t++; k += t;
+	}
+	if (ans == 500010) ans = -1;
+	cout << ans;
 	return 0;
 }
