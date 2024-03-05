7dec79e12a21241b7e8647ee3b3259826a26152c
HanSeo An <gkstj0604@naver.com>
2024-03-05 14:29:03 +0900

    [Gold II] Title: 장난감 조립, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.cc
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.cc
@@ -1,54 +1,49 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <queue>
 using namespace std;
 
-int N, M;
-
-vector<int> base;
-vector<pair<int,int>> v[101];
-int infos[101][101];
-bool visit[101];
-int inDegree[101];
-
-void solve() {
-	int sum = 0;
-	queue<int> q;
-	vector<int> tmp;
-	for (int i = 1; i <= N; i++) {
-		if (inDegree[i] == 0) {
-			q.push(i);
-			base.push_back(i);
-			infos[i][i] = 1;
-		}
-	}
-	while (!q.empty()) {
-		int current = q.front();
-		q.pop();
-		for (int i = 0; i < v[current].size(); i++) {
-			int next = v[current][i].first;
-			inDegree[next]--;
-			for (int j = 0; j < base.size(); j++) {
-				int tmp = base[j];
-				infos[next][tmp] += v[current][i].second * infos[current][tmp];
-			}
-			if (inDegree[next] == 0) {
-				q.push({ next });
-			}
-		}
-	}
-	for (int i = 0; i < base.size(); i++) {
-		cout << base[i] << " " << infos[N][base[i]] << endl;
-	}
-}
+struct Node { int no, need; };
+vector<Node> info[101]; // 조립 정보
+int comb[101]; // 조립 몇 번 되야 하는지 저장
+int cnt[101]; // 부품 사용 횟수
 
 int main() {
-	cin >> N >> M;
-	for (int i = 0; i < M; i++) {
-		int a, b, c;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, t; cin >> n >> t;
+	for (int i = 0; i < t; i++) {
+		int a, b;
+		int c;
 		cin >> a >> b >> c;
-		v[b].push_back({ a,c });
-		inDegree[a]++;
+
+		comb[b]++; // 조립 몇 번 되야 하는지
+		info[a].push_back({ b, c });
 	}
-	solve();
+
+	vector<int> ans;
+	queue<int> q;
+	q.push(n);
+	cnt[n] = 1; // 완성품은 한번 사용
+	while (!q.empty()) {
+		int curr = q.front();
+		q.pop();
+
+		// 조립 정보 없으면 -> 기본 부품
+		if (info[curr].empty()) ans.push_back(curr);
+
+		// 기본 부품 아니면 조립
+		for (auto nxt : info[curr]) {
+			int no = nxt.no; // 조립 해야하는 부품
+			int need = nxt.need; // 사용 개수
+			cnt[no] += cnt[curr] * need; // 필요한 개수 누적
+			if (--comb[no] == 0) q.push(no); // 전부 사용되었으면 push
+		}
+	}
+
+	sort(ans.begin(), ans.end());
+	for (int idx : ans) cout << idx << ' ' << cnt[idx] << '\n';
+	return 0;
 }
