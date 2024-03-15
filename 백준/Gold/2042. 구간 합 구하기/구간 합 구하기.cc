b753e9195035d124e14219d03057ca89796d654e
HanSeo An <gkstj0604@naver.com>
2024-03-15 15:47:05 +0900

    [Gold I] Title: 구간 합 구하기, Time: 216 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,54 +1,77 @@
-#include <bits/stdc++.h>
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-
+#include <iostream>
 using namespace std;
+#define ARRSIZE 1000000
 
-typedef long long ll;
-typedef pair<int, int> pii;
-const double PI = acos(-1);
+long long tree[4 * ARRSIZE];
+long long arr[ARRSIZE];
+int n, m, k;
+int start;
 
-ll maketree(vector<ll> &num, vector<ll> &sgmt, int node, int start, int end){
-	if(start==end) return sgmt[node]=num[start];
-	else{
-		return sgmt[node]=(maketree(num, sgmt, node*2, start, (start+end)/2) + maketree(num, sgmt, (node*2)+1, ((start+end)/2)+1, end));
+void init() {
+
+	start = 2;
+	while (start < n) {
+		start *= 2;
 	}
-}
-
-void change(vector<ll> &num, vector<ll> &sgmt, int node, int start, int end, int loc, ll diff){
-	if(loc<start || loc>end) return;
-	sgmt[node]+=diff;
-	if(start!=end){
-		change(num, sgmt, node*2, start, (start+end)/2, loc, diff);
-		change(num, sgmt, (node*2)+1, ((start+end)/2)+1, end, loc, diff);
+	for (int i = start; i < start + n; i++) {
+		tree[i] = arr[i - start];
 	}
-}
-
-ll sum(vector<ll> &num, vector<ll> &sgmt, int node, int start, int end, int left, int right){
-	if(left>end || right<start) return 0;
-	else if(left<=start && right>=end) return sgmt[node];
-	else{
-		return sum(num, sgmt, node*2, start, (start+end)/2, left, right) + sum(num, sgmt, (node*2)+1, ((start+end)/2)+1, end, left, right);
-	}
-}
-
-int main(){
-	int n,m,k; cin >> n >> m >> k;
-	vector<ll> num, sgmt(n*4);
-	for(int i=0; i<n; i++){
-		ll number; cin >> number;
-		num.pb(number);
-	}
-	maketree(num, sgmt, 1,0,n-1);
-	for(int t=0; t<(m+k); t++){
-		ll a,b,c; cin >> a >> b >> c;
-		if(a==1){
-			ll diff = c-num[b-1];
-			num[b-1]=c;
-			change(num, sgmt, 1, 0, n-1, b-1, diff);
+	for (int i = start / 2; 1 <= i; i /= 2) {
+		for (int j = i; j < i * 2; j++) {
+			tree[j] = tree[j * 2] + tree[j * 2 + 1];
 		}
-		else cout << sum(num, sgmt, 1, 0, n-1, b-1, c-1) << '\n';
 	}
-	return 0;
+}
+
+void update(int index, long long dif) {
+	index += start;
+	tree[index] += dif;
+	index /= 2;
+	while (1 <= index) {
+		tree[index] += dif;
+		index /= 2;
+	}
+}
+
+long long query(int left, int right) {
+	left += start;
+	right += start + 1;
+	long long sum = 0;
+	while (left != right) {
+		if (left % 2 == 1) {
+			sum += tree[left];
+			left++;
+		}
+		if (right % 2 == 1) {
+			right--;
+			sum += tree[right];
+		}
+		left /= 2;
+		right /= 2;
+	}
+	return sum;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	init();
+	for (int i = 0; i < m + k; i++) {
+		int a, b;
+		long long c;
+		cin >> a >> b >> c;
+		if (a == 1) {
+			long long dif = c - arr[b - 1];
+			arr[b - 1] = c;
+			update(b - 1, dif);
+		} 
+		else {
+			cout << query(b - 1, c - 1) << "\n";
+		}
+	}
 }
