6a7743e312ac6edeb30863e128a24c1ab0e4acdf
HanSeo An <gkstj0604@naver.com>
2024-03-22 16:31:08 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,61 +1,59 @@
-#include <bits/stdc++.h>
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-
+#include <iostream>
+#include <vector>
 using namespace std;
+#define INF (1<<31)-1
 
-typedef long long ll;
-typedef pair<int, int> pii;
-const double PI = acos(-1);
-const int IntMax = 2147483647;
+int n, m;
+vector<pair<int, int>> house;
+vector<pair<int, int>> chicken;
+vector<int> selected;
+int res;
 
-int n, m, mx=0, city[51][51], selection[14], ans=IntMax;
-vector<pii> v[3];
-
-int chickenDistance(){
-	int sum=0, sz1=v[1].size(), sz2=v[2].size();
-	for(int i=0; i<sz1; i++){
-		int temp=IntMax;
-		for(int j=0; j<sz2; j++){
-			if(selection[j]==0) continue;
-			temp=min(temp, (abs(v[1][i].first-v[2][j].first)+abs(v[1][i].second-v[2][j].second)));
+int calculate() {
+	int sum = 0;
+	for (int i = 0; i < house.size(); i++) {
+		int dist = INF;
+		for (int j = 0; j < selected.size(); j++) {
+			pair<int, int> p1 = { house[i].first,house[i].second };
+			pair<int, int> p2 = { chicken[selected[j]].first,chicken[selected[j]].second };
+			int temp = abs(p1.first - p2.first) + abs(p1.second - p2.second);
+			dist = min(dist, temp);
 		}
-		sum+=temp;
+		sum += dist;
 	}
 	return sum;
 }
 
-
-void selectChickenHouse(int idx, int cnt){
-	int sz2=v[2].size();
-	if(cnt==m){
-		int curr = chickenDistance();
-		ans=min(ans, curr);
+void solve(int index, int cnt) {
+	if (cnt == m) {
+		res = min(res, calculate());
 		return;
 	}
-	for(int i=idx; i<sz2; i++){
-		if(selection[i]==1) continue;
-		selection[i]=1;
-		city[v[2][i].first][v[2][i].second]=-1;
-		selectChickenHouse(i, cnt+1);
-		selection[i]=0;
-		city[v[2][i].first][v[2][i].second]=2;
+	if (index == chicken.size()) {
+		return;
 	}
+	selected.push_back(index);
+	solve(index + 1, cnt + 1);
+	selected.pop_back();
+	solve(index + 1, cnt);
 }
 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+
+int main() {
+	res = INF;
 	cin >> n >> m;
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
-			cin >> city[i][j];
-			if(city[i][j]==1) v[1].pb(mp(i,j));
-			if(city[i][j]==2) v[2].pb(mp(i,j));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int tmp;
+			cin >> tmp;
+			if (tmp == 1) {
+				house.push_back({ i,j });
+			}
+			else if (tmp == 2) {
+				chicken.push_back({ i,j });
+			}
 		}
 	}
-	selectChickenHouse(0, 0);
-	cout << ans << '\n';
-	return 0;
+	solve(0, 0);
+	cout << res;
 }
